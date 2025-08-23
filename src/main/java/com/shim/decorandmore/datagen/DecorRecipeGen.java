d14e72aaa0d755fe4a184af552a6f363f3899841
Katy <katyschifferer@gmail.com>
2025-08-23 10:38:09 -0700

    table recipes

--- src/main/java/com/shim/decorandmore/datagen/DecorRecipeGen.java
+++ src/main/java/com/shim/decorandmore/datagen/DecorRecipeGen.java
@@ -1,5 +1,7 @@
 package com.shim.decorandmore.datagen;
 
+import com.shim.decorandmore.blocks.LogTableBlock;
+import com.shim.decorandmore.blocks.TableBlock;
 import com.shim.decorandmore.registry.DecorBlocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
@@ -91,10 +93,125 @@ public class DecorRecipeGen extends RecipeProvider {
         redstoneLamp(DecorBlocks.LIGHT_GRAY_REDSTONE_LAMP.get(), Items.LIGHT_GRAY_DYE, consumer);
         redstoneLamp(DecorBlocks.WHITE_REDSTONE_LAMP.get(), Items.WHITE_DYE, consumer);
 
+        table(DecorBlocks.OAK_TABLE_PLANKS.get(), Blocks.OAK_PLANKS, Blocks.OAK_PLANKS, consumer);
+        table(DecorBlocks.OAK_TABLE_LOGS.get(), Blocks.OAK_LOG, Blocks.OAK_LOG, consumer);
+        table(DecorBlocks.OAK_TABLE_STRIPPED_LOGS.get(), Blocks.STRIPPED_OAK_LOG, Blocks.STRIPPED_OAK_LOG, consumer);
+        table(DecorBlocks.OAK_TABLE_PLANKS_LOGS.get(), Blocks.OAK_PLANKS, Blocks.OAK_LOG, consumer);
+        table(DecorBlocks.OAK_TABLE_PLANKS_STRIPPED_LOGS.get(), Blocks.OAK_PLANKS, Blocks.STRIPPED_OAK_LOG, consumer);
+        table(DecorBlocks.OAK_TABLE_STRIPPED_LOGS_LOGS.get(), Blocks.STRIPPED_OAK_LOG, Blocks.OAK_LOG, consumer);
+
+        table(DecorBlocks.DARK_OAK_TABLE_PLANKS.get(), Blocks.DARK_OAK_PLANKS, Blocks.DARK_OAK_PLANKS, consumer);
+        table(DecorBlocks.DARK_OAK_TABLE_LOGS.get(), Blocks.DARK_OAK_LOG, Blocks.DARK_OAK_LOG, consumer);
+        table(DecorBlocks.DARK_OAK_TABLE_STRIPPED_LOGS.get(), Blocks.STRIPPED_DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG, consumer);
+        table(DecorBlocks.DARK_OAK_TABLE_PLANKS_LOGS.get(), Blocks.DARK_OAK_PLANKS, Blocks.DARK_OAK_LOG, consumer);
+        table(DecorBlocks.DARK_OAK_TABLE_PLANKS_STRIPPED_LOGS.get(), Blocks.DARK_OAK_PLANKS, Blocks.STRIPPED_DARK_OAK_LOG, consumer);
+        table(DecorBlocks.DARK_OAK_TABLE_STRIPPED_LOGS_LOGS.get(), Blocks.STRIPPED_DARK_OAK_LOG, Blocks.DARK_OAK_LOG, consumer);
+
+        table(DecorBlocks.SPRUCE_TABLE_PLANKS.get(), Blocks.SPRUCE_PLANKS, Blocks.SPRUCE_PLANKS, consumer);
+        table(DecorBlocks.SPRUCE_TABLE_LOGS.get(), Blocks.SPRUCE_LOG, Blocks.SPRUCE_LOG, consumer);
+        table(DecorBlocks.SPRUCE_TABLE_STRIPPED_LOGS.get(), Blocks.STRIPPED_SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG, consumer);
+        table(DecorBlocks.SPRUCE_TABLE_PLANKS_LOGS.get(), Blocks.SPRUCE_PLANKS, Blocks.SPRUCE_LOG, consumer);
+        table(DecorBlocks.SPRUCE_TABLE_PLANKS_STRIPPED_LOGS.get(), Blocks.SPRUCE_PLANKS, Blocks.STRIPPED_SPRUCE_LOG, consumer);
+        table(DecorBlocks.SPRUCE_TABLE_STRIPPED_LOGS_LOGS.get(), Blocks.STRIPPED_SPRUCE_LOG, Blocks.SPRUCE_LOG, consumer);
+
+        table(DecorBlocks.JUNGLE_TABLE_PLANKS.get(), Blocks.JUNGLE_PLANKS, Blocks.JUNGLE_PLANKS, consumer);
+        table(DecorBlocks.JUNGLE_TABLE_LOGS.get(), Blocks.JUNGLE_LOG, Blocks.JUNGLE_LOG, consumer);
+        table(DecorBlocks.JUNGLE_TABLE_STRIPPED_LOGS.get(), Blocks.STRIPPED_JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG, consumer);
+        table(DecorBlocks.JUNGLE_TABLE_PLANKS_LOGS.get(), Blocks.JUNGLE_PLANKS, Blocks.JUNGLE_LOG, consumer);
+        table(DecorBlocks.JUNGLE_TABLE_PLANKS_STRIPPED_LOGS.get(), Blocks.JUNGLE_PLANKS, Blocks.STRIPPED_JUNGLE_LOG, consumer);
+        table(DecorBlocks.JUNGLE_TABLE_STRIPPED_LOGS_LOGS.get(), Blocks.STRIPPED_JUNGLE_LOG, Blocks.JUNGLE_LOG, consumer);
+
+        table(DecorBlocks.ACACIA_TABLE_PLANKS.get(), Blocks.ACACIA_PLANKS, Blocks.ACACIA_PLANKS, consumer);
+        table(DecorBlocks.ACACIA_TABLE_LOGS.get(), Blocks.ACACIA_LOG, Blocks.ACACIA_LOG, consumer);
+        table(DecorBlocks.ACACIA_TABLE_STRIPPED_LOGS.get(), Blocks.STRIPPED_ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG, consumer);
+        table(DecorBlocks.ACACIA_TABLE_PLANKS_LOGS.get(), Blocks.ACACIA_PLANKS, Blocks.ACACIA_LOG, consumer);
+        table(DecorBlocks.ACACIA_TABLE_PLANKS_STRIPPED_LOGS.get(), Blocks.ACACIA_PLANKS, Blocks.STRIPPED_ACACIA_LOG, consumer);
+        table(DecorBlocks.ACACIA_TABLE_STRIPPED_LOGS_LOGS.get(), Blocks.STRIPPED_ACACIA_LOG, Blocks.ACACIA_LOG, consumer);
+
+        table(DecorBlocks.BIRCH_TABLE_PLANKS.get(), Blocks.BIRCH_PLANKS, Blocks.BIRCH_PLANKS, consumer);
+        table(DecorBlocks.BIRCH_TABLE_LOGS.get(), Blocks.BIRCH_LOG, Blocks.BIRCH_LOG, consumer);
+        table(DecorBlocks.BIRCH_TABLE_STRIPPED_LOGS.get(), Blocks.STRIPPED_BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG, consumer);
+        table(DecorBlocks.BIRCH_TABLE_PLANKS_LOGS.get(), Blocks.BIRCH_PLANKS, Blocks.BIRCH_LOG, consumer);
+        table(DecorBlocks.BIRCH_TABLE_PLANKS_STRIPPED_LOGS.get(), Blocks.BIRCH_PLANKS, Blocks.STRIPPED_BIRCH_LOG, consumer);
+        table(DecorBlocks.BIRCH_TABLE_STRIPPED_LOGS_LOGS.get(), Blocks.STRIPPED_BIRCH_LOG, Blocks.BIRCH_LOG, consumer);
+
+        table(DecorBlocks.WARPED_TABLE_PLANKS.get(), Blocks.WARPED_PLANKS, Blocks.WARPED_PLANKS, consumer);
+        table(DecorBlocks.WARPED_TABLE_STEMS.get(), Blocks.WARPED_STEM, Blocks.WARPED_STEM, consumer);
+        table(DecorBlocks.WARPED_TABLE_STRIPPED_STEMS.get(), Blocks.STRIPPED_WARPED_STEM, Blocks.STRIPPED_WARPED_STEM, consumer);
+        table(DecorBlocks.WARPED_TABLE_PLANKS_STEMS.get(), Blocks.WARPED_PLANKS, Blocks.WARPED_STEM, consumer);
+        table(DecorBlocks.WARPED_TABLE_PLANKS_STRIPPED_STEMS.get(), Blocks.WARPED_PLANKS, Blocks.STRIPPED_WARPED_STEM, consumer);
+        table(DecorBlocks.WARPED_TABLE_STRIPPED_STEMS_STEMS.get(), Blocks.STRIPPED_WARPED_STEM, Blocks.WARPED_STEM, consumer);
+
+        table(DecorBlocks.CRIMSON_TABLE_PLANKS.get(), Blocks.CRIMSON_PLANKS, Blocks.CRIMSON_PLANKS, consumer);
+        table(DecorBlocks.CRIMSON_TABLE_STEMS.get(), Blocks.CRIMSON_STEM, Blocks.CRIMSON_STEM, consumer);
+        table(DecorBlocks.CRIMSON_TABLE_STRIPPED_STEMS.get(), Blocks.STRIPPED_CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM, consumer);
+        table(DecorBlocks.CRIMSON_TABLE_PLANKS_STEMS.get(), Blocks.CRIMSON_PLANKS, Blocks.CRIMSON_STEM, consumer);
+        table(DecorBlocks.CRIMSON_TABLE_PLANKS_STRIPPED_STEMS.get(), Blocks.CRIMSON_PLANKS, Blocks.STRIPPED_CRIMSON_STEM, consumer);
+        table(DecorBlocks.CRIMSON_TABLE_STRIPPED_STEMS_STEMS.get(), Blocks.STRIPPED_CRIMSON_STEM, Blocks.CRIMSON_STEM, consumer);
+
+        simpleTable(DecorBlocks.DIORITE_TABLE.get(), Blocks.DIORITE, consumer);
+        simpleTable(DecorBlocks.DIORITE_TABLE_POLISHED.get(), Blocks.POLISHED_DIORITE, consumer);
+//        simpleTwoBlockTable(DecorBlocks.DIORITE_TABLE_TOP_POLISHED.get(), Blocks.POLISHED_DIORITE, Blocks.DIORITE, consumer);
+//        simpleTwoBlockTable(DecorBlocks.DIORITE_TABLE_BOTTOM_POLISHED.get(), Blocks.DIORITE, Blocks.POLISHED_DIORITE, consumer);
+
+        simpleTable(DecorBlocks.GRANITE_TABLE.get(), Blocks.GRANITE, consumer);
+        simpleTable(DecorBlocks.GRANITE_TABLE_POLISHED.get(), Blocks.POLISHED_GRANITE, consumer);
+        simpleTable(DecorBlocks.ANDESITE_TABLE.get(), Blocks.ANDESITE, consumer);
+        simpleTable(DecorBlocks.ANDESITE_TABLE_POLISHED.get(), Blocks.POLISHED_ANDESITE, consumer);
+
+        simpleTable(DecorBlocks.STONE_BRICKS_TABLE.get(), Blocks.STONE_BRICKS, consumer);
+        simpleTable(DecorBlocks.SMOOTH_STONE_TABLE.get(), Blocks.SMOOTH_STONE, consumer);
+
+        simpleTable(DecorBlocks.QUARTZ_BRICK_TABLE.get(), Blocks.QUARTZ_BRICKS, consumer);
+        simpleTable(DecorBlocks.SMOOTH_QUARTZ_TABLE.get(), Blocks.SMOOTH_QUARTZ, consumer);
+
+        simpleTable(DecorBlocks.NETHER_BRICK_TABLE.get(), Blocks.NETHER_BRICKS, consumer);
+        simpleTable(DecorBlocks.RED_NETHER_BRICK_TABLE.get(), Blocks.RED_NETHER_BRICKS, consumer);
+
+        simpleTable(DecorBlocks.POLISHED_BLACKSTONE_BRICKS_TABLE.get(), Blocks.POLISHED_BLACKSTONE_BRICKS, consumer);
+        simpleTable(DecorBlocks.POLISHED_BLACKSTONE_TABLE.get(), Blocks.POLISHED_BLACKSTONE, consumer);
+
+        simpleTable(DecorBlocks.BRICK_TABLE.get(), Blocks.BRICKS, consumer);
+        simpleTable(DecorBlocks.PURPUR_TABLE.get(), Blocks.PURPUR_BLOCK, consumer);
+        simpleTable(DecorBlocks.END_STONE_BRICKS_TABLE.get(), Blocks.END_STONE_BRICKS, consumer);
+
+        simpleTable(DecorBlocks.CUT_COPPER_TABLE.get(), Blocks.CUT_COPPER, consumer);
+        simpleTable(DecorBlocks.EXPOSED_CUT_COPPER_TABLE.get(), Blocks.EXPOSED_CUT_COPPER, consumer);
+        simpleTable(DecorBlocks.WEATHERED_CUT_COPPER_TABLE.get(), Blocks.WEATHERED_CUT_COPPER, consumer);
+        simpleTable(DecorBlocks.OXIDIZED_CUT_COPPER_TABLE.get(), Blocks.OXIDIZED_CUT_COPPER, consumer);
+
+        simpleTable(DecorBlocks.WAXED_CUT_COPPER_TABLE.get(), Blocks.CUT_COPPER, consumer);
+        simpleTable(DecorBlocks.WAXED_EXPOSED_CUT_COPPER_TABLE.get(), Blocks.EXPOSED_CUT_COPPER, consumer);
+        simpleTable(DecorBlocks.WAXED_WEATHERED_CUT_COPPER_TABLE.get(), Blocks.WEATHERED_CUT_COPPER, consumer);
+        simpleTable(DecorBlocks.WAXED_OXIDIZED_CUT_COPPER_TABLE.get(), Blocks.OXIDIZED_CUT_COPPER, consumer);
+
+        simpleTable(DecorBlocks.CUT_SANDSTONE_TABLE.get(), Blocks.CUT_SANDSTONE, consumer);
+        simpleTable(DecorBlocks.SMOOTH_SANDSTONE_TABLE.get(), Blocks.SMOOTH_SANDSTONE, consumer);
+        simpleTable(DecorBlocks.RED_CUT_SANDSTONE_TABLE.get(), Blocks.CUT_RED_SANDSTONE, consumer);
+        simpleTable(DecorBlocks.SMOOTH_RED_SANDSTONE_TABLE.get(), Blocks.SMOOTH_SANDSTONE, consumer);
+
+        simpleTable(DecorBlocks.POLISHED_DEEPSLATE_TABLE.get(), Blocks.POLISHED_DEEPSLATE, consumer);
+        simpleTable(DecorBlocks.DEEPSLATE_BRICK_TABLE.get(), Blocks.DEEPSLATE_BRICKS, consumer);
+        simpleTable(DecorBlocks.DEEPSLATE_TILE_TABLE.get(), Blocks.DEEPSLATE_TILES, consumer);
+
+        simpleTable(DecorBlocks.PRISMARINE_BRICK_TABLE.get(), Blocks.PRISMARINE_BRICKS, consumer);
+        simpleTable(DecorBlocks.DARK_PRISMARINE_TABLE.get(), Blocks.DARK_PRISMARINE, consumer);
+
+
     }
 
     private String name(ItemLike block) {
         return block.asItem().getRegistryName().getPath();
+    }
+
+
+    public void simpleTable(TableBlock table, ItemLike craftingItem, Consumer<FinishedRecipe> consumer) {
+        table(table, craftingItem, craftingItem, consumer);
+    }
+
+
+    public void table(TableBlock table, ItemLike topItem, ItemLike bottomItem, Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(table, 4).pattern("XXX").pattern(" x ").pattern(" x ").define('X', topItem).define('x', bottomItem).unlockedBy(name(topItem), has(topItem)).save(consumer);
     }
 
     public void redstoneLamp(ItemLike lamp, Item dye, Consumer<FinishedRecipe> consumer) {
